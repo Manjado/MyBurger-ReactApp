ead39fde1b44891f27164f048a99c10cd7f2c4b4
Manjado <michalalchimowicz@gmail.com>
2018-01-24 23:10:33 +0100

    contactData

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	console.log(props)
 	let transformedIngredients = Object.keys(props.ingredients)
 		.map(igKey => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -27,4 +25,4 @@ const burger = (props) => {
 	);
 };
 
-export default withRouter(burger);
+export default burger;

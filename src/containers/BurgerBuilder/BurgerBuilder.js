009d355a36fde0c7962a66e33c0bbdde8ba3fae3
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-02-19 17:42:47 +0100

    add reducer with deeply clon

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,13 +28,13 @@ class BurgerBuilder extends Component {
 	}
 
 	componentDidMount () {
-		axios.get('https://react-my-burger-c7214.firebaseio.com/ingredients.json')
+/*		axios.get('https://react-my-burger-c7214.firebaseio.com/ingredients.json')
 			.then(response => {
 				this.setState({ingredients: response.data})
 			})
 			.catch(error => {
 				this.setState({error: true})
-			});
+			});*/
 	}
 
 	updatePurchaseState (ingredients) {
